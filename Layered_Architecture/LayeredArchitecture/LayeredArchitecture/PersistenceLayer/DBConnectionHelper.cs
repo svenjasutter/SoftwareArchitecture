bdc8f8b9cb34a459d08ca5ac0c42d00ea4ee9742
svenjasutter <svenja.sutter2@gmail.com>
2022-07-27 11:24:40 +0200

    Layered Architecture Summary

---
+++ Layered_Architecture/LayeredArchitecture/LayeredArchitecture/PersistenceLayer/DBConnectionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LayeredArchitecture.PersistenceLayer
+{
+    internal static class DBConnectionHelper
+    {
+        private static SqlConnection con;
+        public static SqlConnection CreateSqlConnection()
+        {
+            string ConnectionString = "Server=localhost;Initial Catalog=LayeredArchitecture;Integrated Security=true";
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+            return con;
+        }
+
+        public static SqlConnection GetSqlConnection()
+        {
+            return con;
+        }
+        public static void CloseSqlConnection()
+        {
+            con.Close();
+        }
+    }
+}
---
+++ Layered_Architecture/LayeredArchitecture/LayeredArchitecture/PersistenceLayer/TaskHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LayeredArchitecture.PersistenceLayer
+{
+    internal class TaskHelper
+    {
+        public void GetTasks()
+        {
+            SqlCommand cmd = new SqlCommand("select * from LayeredArchitecture..Task");
+            cmd.Connection = DBConnectionHelper.GetSqlConnection();
+
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("Data from Reader:");
+                Console.WriteLine(String.Format("{0}", reader[0]));
+            }
+        }
+    }
+}
